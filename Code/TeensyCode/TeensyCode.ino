e77b5a804ecdd0fb3a4ad46857a4ae179c793ec9
Armin Oonk <armin@berkelaarmrt.com>
2015-05-25 17:46:52 +0200

    First test code

--- Code/TeensyCode/TeensyCode.ino
+++ Code/TeensyCode/TeensyCode.ino
@@ -6,20 +6,80 @@ int Working = 4;
 int LowSupplies = 9;
 int Thirsty = 6;
 
-int Pump = 10;
+int PumpPin = 10;
+unsigned PumpTimeout = 60*60*1000; // 1 Hour
+unsigned PumpInitTime = 60*1000;
+bool FirstPump = true;
+unsigned PumpTime = 20*1000; // Pump for 20 seconds
+bool isPumpOn = false;
+bool PumpManual = false;
+
+// Moisture measurement not connected --> >1000
+// Higher is drieer
+int StartPump = 450;
+unsigned LastPumpTime = 0;
+
+void PumpOn()
+{
+  if(!isPumpOn)
+  {
+    Serial.println("Pumping...");
+    LastPumpTime = millis(); // First time we turn the pump on, save time
+     
+    analogWrite(Working, 255);
+    isPumpOn = true;
+     
+    digitalWrite(PumpPin, HIGH); 
+  }
+}
+
+void PumpOff()
+{
+  digitalWrite(PumpPin, LOW); 
+  analogWrite(Working, 0);
+  isPumpOn = false;
+}
+
+void CheckMoisture()
+{
+  int moist = analogRead(A0);
+  Serial.print("Moisture: ");
+  Serial.println(moist);
+  
+  if(moist > StartPump)
+  {
+    analogWrite(Thirsty, 255);
+    // Do not start pumping the first 5 minutes
+    if(millis() > PumpInitTime && FirstPump)
+    {
+      FirstPump = false;
+      PumpOn();
+    }
+    
+    if(millis() > (LastPumpTime + PumpTimeout) )
+    {
+      // Start pump
+      PumpOn();
+    }  
+  }
+  else
+  {
+    analogWrite(Thirsty, 0);
+  }
+}
+
 
 // the setup routine runs once when you press reset:
 void setup() 
 {                
   // initialize the digital pin as an output.
   pinMode(OnboardLed, OUTPUT);   
-  pinMode(Pump, OUTPUT);
+  pinMode(PumpPin, OUTPUT);
 
   // Button
   pinMode(Button, INPUT_PULLUP);
 
   Serial.begin(115200);
-  
 }
 
 bool advertise = true;
@@ -38,15 +98,20 @@ void loop()
   
   if (digitalRead(Button))
   {
-    digitalWrite(Pump, LOW);
+    CheckMoisture();
+    
+    if(isPumpOn && millis() > (LastPumpTime + PumpTime) || PumpManual)
+    {
+      PumpOff(); // long enough
+      PumpManual = false;
+    }
+    
+    
     // Button Led fading (Show alive)
     digitalWrite(OnboardLed, HIGH);
     for (int fadeValue = 0 ; fadeValue < COR_LENGTH; fadeValue++) 
     {
       analogWrite(ButtonLed, fadeCor[fadeValue]);
-      analogWrite(Working, fadeCor[fadeValue]);
-      analogWrite(LowSupplies, fadeCor[fadeValue]);
-      analogWrite(Thirsty, fadeCor[fadeValue]);
       delay(40);
     }
   
@@ -55,17 +120,17 @@ void loop()
     for (int fadeValue = COR_LENGTH-1 ; fadeValue >= 0; fadeValue--) 
     {
       analogWrite(ButtonLed, fadeCor[fadeValue]);
-      analogWrite(Working, fadeCor[fadeValue]);
-      analogWrite(LowSupplies, fadeCor[fadeValue]);
-      analogWrite(Thirsty, fadeCor[fadeValue]);
       delay(40);
     }
   }
   else
   {
-    // Pumping?
+    // Manual
+    PumpManual = true;
     analogWrite(ButtonLed, fadeCor[COR_LENGTH-1]);
     digitalWrite(OnboardLed, HIGH);
-    digitalWrite(Pump, HIGH);
+    PumpOn();
   }
+  
+  
 }
